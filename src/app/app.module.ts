875b4c4a15f69778e82688c56adc5e5cf344eeaa
wow <theoblystedt@gmail.com>
2023-03-21 10:54:57 +0100

    Changes by jeffbengtsson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import {FormsModule} from "@angular/forms";
+import {FirebaseService} from "./services/firebase.service";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    FormsModule
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'angularassignment2-69539',
+    appId: '1:521725264119:web:0990933d3ab61d1501d3f2',
+    storageBucket: 'angularassignment2-69539.appspot.com',
+    apiKey: 'AIzaSyA4XjfzUUD1uzTmwrqG2ceHS6jBkoochVA',
+    authDomain: 'angularassignment2-69539.firebaseapp.com',
+    messagingSenderId: '521725264119',
+    measurementId: 'G-S6K1QNXDP4',
+  },};
